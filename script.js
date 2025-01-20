0f0b7dda86f074150349ad97a792ee80ab9c6977
ClinnFN <sharilynnb15@gmail.com>
2025-01-20 17:58:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Open Modal
+function openModal() {
+    document.querySelector('.modal').style.display = 'flex';
+}
+
+// Close Modal
+function closeModal() {
+    document.querySelector('.modal').style.display = 'none';
+}
